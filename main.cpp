fbb90b3cf16b5f71c70ce0a84c051a4ca29c6956
Dave Compton <to.dave.c@gmail.com>
2011-02-16 17:05:58 -0800

    cleaned up some

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
-// crt_contrlfp_s.c
-// processor: x86
-// This program uses _controlfp_s to output the FP control 
-// word, set the precision to 24 bits, and reset the status to 
-// the default.
-//
 #include <iostream>
 #include <float.h>
-#pragma fenv_access (on) // Disables (ON) or enables (OFF) optimizations that could change flag tests and mode changes.
+
+// Disables (ON) or enables (OFF) optimizations 
+// that could change flag tests and mode changes.
+#pragma fenv_access (on) 
 
 using namespace std;
+
 static void tweak_fp_control(unsigned int new_value, unsigned int mask)
 {
-    // Show original FP control word
     unsigned int control_word;
     unsigned int err = _controlfp_s(&control_word, new_value, mask);
     if ( err ) {
-        cerr << "error " << err << " when calling _controlfp_s( new_value = " << new_value << ", mask = " << mask << " )" << endl;
+        cerr << "error " << err 
+             << " when calling _controlfp_s( new_value = " 
+             << new_value << ", mask = " << mask << " )" << endl;
     } else
     {
-        cout << "successfully called _controlfp_s( new_value = " << new_value << ", mask = " << mask << " )" << endl
+        cout << "successfully called _controlfp_s( new_value = " 
+             << new_value << ", mask = " << mask << " )" << endl
              << "new control word = " << control_word << endl;
     }
 }
@@ -28,14 +28,26 @@ int main( void )
 {
     cout << hex;
     cerr << hex;
+
     double a = 0.1;
     double b=0.1;
     double c;
 
-    tweak_fp_control(0, 0);
+    // don't set anything but print the current value
+    tweak_fp_control(0, 0);  
     // c = 1.0/(a-b);
-    tweak_fp_control(0, _EM_ZERODIVIDE);
 
-    c = 1.0/(a-b);
+    // Enable program termination when dividing by zero.
+    // This call errors immediately if there already was a divide by zero.
+    tweak_fp_control(0, _EM_ZERODIVIDE);  
+
+    try {
+        // This gets caught in the debugger.  
+        // When running from a dos window it just exits.
+        c = 1.0/(a-b);  
+    } catch(...) {
+        cerr << "error" << std::endl;  // This does not get hit.
+    }
+    cerr << "hello world\n" << endl;
     return 0;
 }
